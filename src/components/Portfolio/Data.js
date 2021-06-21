67317daf88610063cf760b316e122e3076211ce1
tedlopez <tedmlopez@gmail.com>
2021-06-20 22:06:24 -0400

    Add scouted to portfolio

--- src/components/Portfolio/Data.js
+++ src/components/Portfolio/Data.js
@@ -5,8 +5,7 @@ export const pitcherTrendsObj = {
   subtitle: "I think his arm's about to fall off!",
   image: require("../../images/portfolio/pitchertrends.png").default,
   thumbnail: require("../../images/thumbnails/pitchertrends-thumb.png").default,
-  desc:
-    "A simple Ruby on Rails data visualization application that displays baseball pitcher trends over the course of a season. It can help highlight potential performance irregularities or health risks. Built using Ruby on Rails, Postgresql, and JavaScript library ChartJS.",
+  desc: "A simple Ruby on Rails data visualization application that displays baseball pitcher trends over the course of a season. It can help highlight potential performance irregularities or health risks. Built using Ruby on Rails, Postgresql, and JavaScript library ChartJS.",
   tech: ["Ruby on Rails", "Postgresql", "ChartJS"],
   url: "https://pitcher-trends.herokuapp.com/",
   git: "https://github.com/teddylopez/pitcher_trends",
@@ -19,8 +18,7 @@ export const amazonObj = {
   subtitle: "A less evil Amazon made with React",
   image: require("../../images/portfolio/amazon.png").default,
   thumbnail: require("../../images/thumbnails/amazon-thumb.png").default,
-  desc:
-    "This is a basic functionality ecommerce website built using React for the frontend, Firebase for the backend database, and stripe for payment processing. I built it primarily to learn and practice basic frontend development and styling with React",
+  desc: "This is a basic functionality ecommerce website built using React for the frontend, Firebase for the backend database, and stripe for payment processing. I built it primarily to learn and practice basic frontend development and styling with React",
   tech: ["React", "Firebase", "Stripe"],
   url: "https://fir-6bf26.web.app/",
   git: "https://github.com/teddylopez/amazon-clone",
@@ -33,8 +31,7 @@ export const tedSurfObj = {
   subtitle: "A Vue.js surf report application",
   image: require("../../images/portfolio/tedsurf.png").default,
   thumbnail: require("../../images/thumbnails/tedsurf-thumb.png").default,
-  desc:
-    "It's firing! This simple desktop and mobile friendly Vue.js application gives you the surf forecast for three of my favorite surfing destinations. It retrieves data from a mainstream surf forecast provider's API that doesn't seem to be very secure.",
+  desc: "It's firing! This simple desktop and mobile friendly Vue.js application gives you the surf forecast for three of my favorite surfing destinations. It retrieves data from a mainstream surf forecast provider's API that doesn't seem to be very secure.",
   tech: ["Vue.js", "JavaScript", "Responsive Design"],
   url: "https://surf-vue.herokuapp.com/",
   git: "https://github.com/teddylopez/vue-surf",
@@ -47,8 +44,7 @@ export const monthlyVueObj = {
   subtitle: "Let's reschedule... a calendar application using Vue.js",
   image: require("../../images/portfolio/monthlyvue.png").default,
   thumbnail: require("../../images/thumbnails/monthlyvue-thumb.png").default,
-  desc:
-    "What's the action? Check your schedge with this calendar app built with Vue.js. It makes use of Vuetify for frontend styling and Google Firebase for it's backend database.",
+  desc: "What's the action? Check your schedge with this calendar app built with Vue.js. It makes use of Vuetify for frontend styling and Google Firebase for it's backend database.",
   tech: ["Vue.js", "Vuetify", "Firebase"],
   url: "https://monthly-vue.herokuapp.com/",
   git: "https://github.com/teddylopez/monthly-vue",
@@ -61,8 +57,7 @@ export const depthChartObj = {
   subtitle: "Who's on third?",
   image: require("../../images/portfolio/depthchart.png").default,
   thumbnail: require("../../images/thumbnails/depthchart-thumb.png").default,
-  desc:
-    "Drag and drop players and rearrange them however you want to build your team! Depth charts are updated and saved to the backend via an AJAX request that is triggered when the chart gets updated.",
+  desc: "Drag and drop players and rearrange them however you want to build your team! Depth charts are updated and saved to the backend via an AJAX request that is triggered when the chart gets updated.",
   tech: ["Ruby on Rails", "JavaScript"],
   url: "https://depth-chart.herokuapp.com/",
   git: "https://github.com/teddylopez/depth-chart",
@@ -88,8 +83,7 @@ export const teddygramsObj = {
   subtitle: "Photo sharing for Ted",
   image: require("../../images/portfolio/teddygrams.png").default,
   thumbnail: require("../../images/thumbnails/teddygrams-thumb.png").default,
-  desc:
-    "teddygrams is a image sharing application that was built using React. I also incorporated Redux into the project to manage component state.",
+  desc: "teddygrams is a image sharing application that was built using React. I also incorporated Redux into the project to manage component state.",
   tech: ["React", "Redux"],
   url: false,
   git: "https://github.com/teddylopez/teddygrams",
@@ -102,8 +96,7 @@ export const tednessObj = {
   subtitle: "A music player app",
   image: require("../../images/portfolio/tedness.png").default,
   thumbnail: require("../../images/thumbnails/tedness-thumb.png").default,
-  desc:
-    "tedness is another project from Bloc's website development class. It's a music player site, built using the React JS library. It was my first experience building a full project with React. When I took the Bloc course, we built this project multiple times: first with plain Javascript, then with incorporated jQuery, and then in Angular. However after completing the course, the curriculum changed -- substituting Angular with React. I've been interested in React since first hearing about it and had experimented with it in my free time. I decided to continue with this new part of the curriculum, and build this project after having already completed the class. Learning a new language or framework can take time. I spent about a week learning about React before jumping into this project, and having already worked with Angular I found it fairly similar. I like the idea of single page applications, encapsulating components that manage their own state. Programming in general is all about breaking down problems into manageable pieces, which is fundamental to SPAs and React. The project consists of a landing page, a library page which holds all of the albums, and then the album page. I spent a significant amount of time styling the pages to give them a unique, clean look, while using Bootstrap to assist in making it responsive. I'm looking forward to building more robust applications with Redux in the near future.",
+  desc: "tedness is another project from Bloc's website development class. It's a music player site, built using the React JS library. It was my first experience building a full project with React. When I took the Bloc course, we built this project multiple times: first with plain Javascript, then with incorporated jQuery, and then in Angular. However after completing the course, the curriculum changed -- substituting Angular with React. I've been interested in React since first hearing about it and had experimented with it in my free time. I decided to continue with this new part of the curriculum, and build this project after having already completed the class. Learning a new language or framework can take time. I spent about a week learning about React before jumping into this project, and having already worked with Angular I found it fairly similar. I like the idea of single page applications, encapsulating components that manage their own state. Programming in general is all about breaking down problems into manageable pieces, which is fundamental to SPAs and React. The project consists of a landing page, a library page which holds all of the albums, and then the album page. I spent a significant amount of time styling the pages to give them a unique, clean look, while using Bootstrap to assist in making it responsive. I'm looking forward to building more robust applications with Redux in the near future.",
   tech: ["JavaScript", "Angular", "React", "Bootstrap"],
   url: "https://tedness.herokuapp.com/",
   git: "https://github.com/teddylopez/tedness",
@@ -116,8 +109,7 @@ export const tedTalksObj = {
   subtitle: "My first portfolio website",
   image: require("../../images/portfolio/tedtalks.png").default,
   thumbnail: require("../../images/thumbnails/tedtalks-thumb.png").default,
-  desc:
-    "This was the first portfolio website I made. I spent a lot of time building a website with a simple, clean, and streamlined look.",
+  desc: "This was the first portfolio website I made. I spent a lot of time building a website with a simple, clean, and streamlined look.",
   tech: ["Ruby on Rails"],
   url: "https://tedlopez-portfolio.herokuapp.com/",
   git: "https://github.com/teddylopez/portfolio",
@@ -143,8 +135,7 @@ export const tedmetricsObj = {
   subtitle: "Like Google Analytics if it was made by Ted",
   image: require("../../images/portfolio/tedmetrics.png").default,
   thumbnail: require("../../images/thumbnails/tedmetrics-thumb.png").default,
-  desc:
-    "tedmetrics is a website analytics project that was also part of the Bloc curriculum. Users are able to insert a code snippet within an application that they would like to track and view the data within this application. It uses the Chartick and Groupdate gems to display those images in colorful graphs on the application show pages. A lot of my day-to-day at my current profession is analyzing website data, and I've always been interested in learning how those tools work. Users insert a custom JavaScript snippet into projects of their choice, which fires off a JSON object when an event is triggered. An external API call is made, and the event is recorded within tedmetrics. Feel free to look around the project by using the login: username: ted@tedmetrics.com password: password",
+  desc: "tedmetrics is a website analytics project that was also part of the Bloc curriculum. Users are able to insert a code snippet within an application that they would like to track and view the data within this application. It uses the Chartick and Groupdate gems to display those images in colorful graphs on the application show pages. A lot of my day-to-day at my current profession is analyzing website data, and I've always been interested in learning how those tools work. Users insert a custom JavaScript snippet into projects of their choice, which fires off a JSON object when an event is triggered. An external API call is made, and the event is recorded within tedmetrics. Feel free to look around the project by using the login: username: ted@tedmetrics.com password: password",
   tech: ["Ruby on Rails", "JavaScript", "ChartJS"],
   url: "https://tedmetrics.herokuapp.com/",
   git: "https://github.com/teddylopez/tedmetrics",
@@ -157,8 +148,7 @@ export const tedWeathermanObj = {
   subtitle: "It can't be that cold out there",
   image: require("../../images/portfolio/tedweatherman.png").default,
   thumbnail: require("../../images/thumbnails/tedweatherman-thumb.png").default,
-  desc:
-    "Forget the umbrella! This React application tells you the weather for whatever location you enter by fetching data from the OpenWeatherMap API.",
+  desc: "Forget the umbrella! This React application tells you the weather for whatever location you enter by fetching data from the OpenWeatherMap API.",
   tech: ["JavaScript", "React"],
   url: "https://ted-weatherman.herokuapp.com/",
   git: "https://github.com/teddylopez/weatherman",
@@ -171,8 +161,7 @@ export const tedspaceObj = {
   subtitle: "My second portfolio website",
   image: require("../../images/portfolio/tedspace.png").default,
   thumbnail: require("../../images/thumbnails/tedspace-thumb.png").default,
-  desc:
-    "tedspace is my second portfolio website built using Ruby on Rails and Postgresql. Photos and videos are hosted on AWS, blog posts were rarely updated, and there's an entire page dedicated to the worst of twitter.",
+  desc: "tedspace is my second portfolio website built using Ruby on Rails and Postgresql. Photos and videos are hosted on AWS, blog posts were rarely updated, and there's an entire page dedicated to the worst of twitter.",
   tech: ["Ruby on Rails", "Postgresql", "AWS", "TwitterAPI"],
   url: "https://tedspace-redesign.herokuapp.com/",
   git: "https://github.com/teddylopez/tedspace",
@@ -186,8 +175,7 @@ export const tedspaceReactObj = {
   image: require("../../images/portfolio/tedspace-react.png").default,
   thumbnail: require("../../images/thumbnails/tedspace-react-thumb.png")
     .default,
-  desc:
-    "I rebuilt my last portfolio website into a concise, mobile-friendly and responsive website using React. I made use of the popular 'styled-components' React module to style the entire project.",
+  desc: "I rebuilt my last portfolio website into a concise, mobile-friendly and responsive website using React. I made use of the popular 'styled-components' React module to style the entire project.",
   tech: ["React", "JavaScript"],
   url: "http://tedlopez.us/",
   git: "https://github.com/teddylopez/tedspace_v3",
@@ -200,8 +188,7 @@ export const tedtrackerObj = {
   subtitle: "An enterprise-ready time management application",
   image: require("../../images/portfolio/tedtracker.png").default,
   thumbnail: require("../../images/thumbnails/tedtracker-thumb.png").default,
-  desc:
-    "Big Brother Ted is watching. Enter in your weekly hours or get fired! This time-management software built with Ruby on Rails and Postgresql allows employees to enter weekly billable hours or request overtime while administrators can approve or reject each submission through an admin dashboard. Custom-built tasks remind users via email to enter in all of their time by the end of the week, while admins recieve text messages when there are outstanding employee submissions that need apporoval. Get back to work!",
+  desc: "Big Brother Ted is watching. Enter in your weekly hours or get fired! This time-management software built with Ruby on Rails and Postgresql allows employees to enter weekly billable hours or request overtime while administrators can approve or reject each submission through an admin dashboard. Custom-built tasks remind users via email to enter in all of their time by the end of the week, while admins recieve text messages when there are outstanding employee submissions that need apporoval. Get back to work!",
   tech: ["Ruby on Rails", "Postgresql", "Twilio API"],
   url: false,
   git: "https://github.com/teddylopez/tedtracker",
@@ -214,8 +201,7 @@ export const covidTrackerObj = {
   subtitle: ":/",
   image: require("../../images/portfolio/covid.png").default,
   thumbnail: require("../../images/thumbnails/covid-thumb.png").default,
-  desc:
-    "This desktop and mobile friendly data visualization app uses ChartJS and Leaflet to display worldwide COVID-19 data provided by the disease.sh API. I don't know what to believe anymore!",
+  desc: "This desktop and mobile friendly data visualization app uses ChartJS and Leaflet to display worldwide COVID-19 data provided by the disease.sh API. I don't know what to believe anymore!",
   tech: ["React", "Firebase", "ChartJS", "Leaflet", "Responsive design"],
   url: "https://covid-tracker-4acd6.web.app/",
   git: "https://github.com/teddylopez/covid-tracker",
@@ -228,8 +214,7 @@ export const trailerHitchObj = {
   subtitle: "I need a new toy!",
   image: require("../../images/portfolio/trailerhitch.png").default,
   thumbnail: require("../../images/thumbnails/trailerhitch-thumb.png").default,
-  desc:
-    "trailer_hitch is a Ruby on Rails application that scrapes the popular vehicle auction website bringatrailer.com. I built a basic frontend to view upcoming auctions, but the primary purpose of the app is to compile them in an email that gets sent to my inbox every morning and evening when there are listings that expire within the next 12 hours that are under a certain pricepoint.",
+  desc: "trailer_hitch is a Ruby on Rails application that scrapes the popular vehicle auction website bringatrailer.com. I built a basic frontend to view upcoming auctions, but the primary purpose of the app is to compile them in an email that gets sent to my inbox every morning and evening when there are listings that expire within the next 12 hours that are under a certain pricepoint.",
   tech: ["Ruby on Rails"],
   url: "https://trailerhitch.herokuapp.com/",
   git: "https://github.com/teddylopez/trailer_hitch",
@@ -242,9 +227,21 @@ export const statLines = {
   subtitle: "Good day at the plate",
   image: require("../../images/portfolio/statlines.png").default,
   thumbnail: require("../../images/thumbnails/statlines-thumb.png").default,
-  desc:
-    "Stat Lines is a basic JavaScript application that displays baseball player stats for a given game. Data is initially fetched from the backend on page-load, and uses 'infinite scrolling' that fetches more data when a user scrolls to the bottom of the table. The stat table itself is a reusable component that has the flexibilty to render different stat types depending on the player.",
+  desc: "Stat Lines is a basic JavaScript application that displays baseball player stats for a given game. Data is initially fetched from the backend on page-load, and uses 'infinite scrolling' that fetches more data when a user scrolls to the bottom of the table. The stat table itself is a reusable component that has the flexibilty to render different stat types depending on the player.",
   tech: ["React", "Node.js", "Postgresql"],
   url: "https://stat-lines.herokuapp.com/",
   git: "https://github.com/teddylopez/stat-lines",
+};
+
+export const scouted = {
+  id: 19,
+  name: "scouted",
+  title: "scouted",
+  subtitle: "Not another baseball project!",
+  image: require("../../images/portfolio/scouted.png").default,
+  thumbnail: require("../../images/thumbnails/scouted-thumb.png").default,
+  desc: "scouted is an ongoing baseball project that was originally built with Ruby on Rails. I've recently been transitioning towards Elixir and it's related framework Phoenix and have used this project as my main method of learning both. Learning new languages, technologies, and paradigms like functional programming, Ecto, and LiveView has been both, challenging and really fun. I think Elixir and Phoenix can make a huge impact on website development, and I'm excited to work more with both. JavaScript frameworks suck, so let's hope Elixir, Phoenix, and LiveView take off. Take a look inside with the login 'guest@scouted.com', and password: 'password'.",
+  tech: ["Phoenix", "Elixir", "tailwindcss", "Alpine.js", "LiveView"],
+  url: "https://scouted.herokuapp.com/",
+  git: "https://github.com/teddylopez/scouted",
 };
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,60 +26,169 @@ import {
   covidTrackerObj,
   trailerHitchObj,
   statLines,
+  scouted
 } from "./Data";
 
 function Portfolio() {
-  return (
-    <>
-      <PageWrapper>
-        <PortfolioPage>
-          <PortfolioRow>
-            <ItemSection {...statLines} portfolio_object={statLines} />
-            <ItemSection
-              {...covidTrackerObj}
-              portfolio_object={covidTrackerObj}
-            />
-            <ItemSection
-              {...pitcherTrendsObj}
-              portfolio_object={pitcherTrendsObj}
-            />
-            <ItemSection {...tedSurfObj} portfolio_object={tedSurfObj} />
-          </PortfolioRow>
-          <PortfolioRow>
-            <ItemSection
-              {...trailerHitchObj}
-              portfolio_object={trailerHitchObj}
-            />
-            <ItemSection {...tedtrackerObj} portfolio_object={tedtrackerObj} />
-            <ItemSection {...amazonObj} portfolio_object={amazonObj} />
-            <ItemSection {...depthChartObj} portfolio_object={depthChartObj} />
-          </PortfolioRow>
-          <PortfolioRow>
-            <ItemSection {...tedTalksObj} portfolio_object={tedTalksObj} />
-            <ItemSection {...tedspaceObj} portfolio_object={tedspaceObj} />
-            <ItemSection
-              {...tedspaceReactObj}
-              portfolio_object={tedspaceReactObj}
-            />
-          </PortfolioRow>
-          <PortfolioRow>
-            <ItemSection {...beatsByTedObj} portfolio_object={beatsByTedObj} />
-            <ItemSection {...tedmetricsObj} portfolio_object={tedmetricsObj} />
-            <ItemSection {...tednessObj} portfolio_object={tednessObj} />
-            <ItemSection
-              {...tedWeathermanObj}
-              portfolio_object={tedWeathermanObj}
-            />
-          </PortfolioRow>
-          <PortfolioRow>
-            <ItemSection {...monthlyVueObj} portfolio_object={monthlyVueObj} />
-            <ItemSection {...tedTypeObj} portfolio_object={tedTypeObj} />
-            <ItemSection {...teddygramsObj} portfolio_object={teddygramsObj} />
-          </PortfolioRow>
-        </PortfolioPage>
-        <Footer />
-      </PageWrapper>
-    </>
+  return ( <
+    >
+    <
+    PageWrapper >
+    <
+    PortfolioPage >
+    <
+    PortfolioRow >
+    <
+    ItemSection {
+      ...scouted
+    }
+    portfolio_object = {
+      scouted
+    }
+    /> <
+    ItemSection {
+      ...statLines
+    }
+    portfolio_object = {
+      statLines
+    }
+    /> <
+    ItemSection {
+      ...pitcherTrendsObj
+    }
+    portfolio_object = {
+      pitcherTrendsObj
+    }
+    /> <
+    ItemSection {
+      ...tedSurfObj
+    }
+    portfolio_object = {
+      tedSurfObj
+    }
+    /> < /
+    PortfolioRow > <
+    PortfolioRow >
+    <
+    ItemSection {
+      ...trailerHitchObj
+    }
+    portfolio_object = {
+      trailerHitchObj
+    }
+    /> <
+    ItemSection {
+      ...tedtrackerObj
+    }
+    portfolio_object = {
+      tedtrackerObj
+    }
+    /> <
+    ItemSection {
+      ...covidTrackerObj
+    }
+    portfolio_object = {
+      covidTrackerObj
+    }
+    /> <
+    ItemSection {
+      ...depthChartObj
+    }
+    portfolio_object = {
+      depthChartObj
+    }
+    /> < /
+    PortfolioRow > <
+    PortfolioRow >
+    <
+    ItemSection {
+      ...tedTalksObj
+    }
+    portfolio_object = {
+      tedTalksObj
+    }
+    /> <
+    ItemSection {
+      ...tedspaceObj
+    }
+    portfolio_object = {
+      tedspaceObj
+    }
+    /> <
+    ItemSection {
+      ...tedspaceReactObj
+    }
+    portfolio_object = {
+      tedspaceReactObj
+    }
+    /> <
+    ItemSection {
+      ...amazonObj
+    }
+    portfolio_object = {
+      amazonObj
+    }
+    />  < /
+    PortfolioRow > <
+    PortfolioRow >
+    <
+    ItemSection {
+      ...beatsByTedObj
+    }
+    portfolio_object = {
+      beatsByTedObj
+    }
+    /> <
+    ItemSection {
+      ...tedmetricsObj
+    }
+    portfolio_object = {
+      tedmetricsObj
+    }
+    /> <
+    ItemSection {
+      ...tednessObj
+    }
+    portfolio_object = {
+      tednessObj
+    }
+    /> <
+    ItemSection {
+      ...tedWeathermanObj
+    }
+    portfolio_object = {
+      tedWeathermanObj
+    }
+    /> < /
+    PortfolioRow > <
+    PortfolioRow >
+    <
+    ItemSection {
+      ...monthlyVueObj
+    }
+    portfolio_object = {
+      monthlyVueObj
+    }
+    /> <
+    ItemSection {
+      ...tedTypeObj
+    }
+    portfolio_object = {
+      tedTypeObj
+    }
+    /> <
+    ItemSection {
+      ...teddygramsObj
+    }
+    portfolio_object = {
+      teddygramsObj
+    }
+    /> < /
+    PortfolioRow > <
+    /PortfolioPage> <
+    Footer / >
+    <
+    /PageWrapper> < / >
   );
 }
 
